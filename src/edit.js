4c5d336039281dbd8fff0855ee8ac68881d24a84
Andrii Tsyhaniuk <in0mad91@gmail.com>
2024-05-08 19:41:39 +0300

    feat: Implements taxonomy terms display with wrapper variants and links

--- src/edit.js
+++ src/edit.js
@@ -1,41 +1,96 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
-
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { SelectControl, PanelBody, ToggleControl } from '@wordpress/components';
+import { useDispatch } from '@wordpress/data';
+import { useEffect, useState } from '@wordpress/element';
 import './editor.scss';
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit() {
-	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Taxonomy List Block – hello from the editor!',
-				'taxonomy-list-block'
-			) }
-		</p>
-	);
+export default function Edit( { attributes, setAttributes } ) {
+    const { selectedTaxonomy, outputVariant, includeLinks } = attributes;
+    const { editPost } = useDispatch( 'core/editor' );
+    const [ taxonomies, setTaxonomies ] = useState( [] );
+    const [ terms, setTerms ] = useState( [] );
+
+    // Fetch taxonomies
+    useEffect( () => {
+        wp.apiFetch( { path: '/wp/v2/taxonomies' } ).then( ( data ) => {
+            setTaxonomies( data );
+        } );
+    }, [] );
+
+    // Fetch terms when selectedTaxonomy changes
+    useEffect( () => {
+        if ( selectedTaxonomy ) {
+            wp.apiFetch( { path: `/wp/v2/${ selectedTaxonomy }` } ).then( ( data ) => {
+                setTerms( data );
+            } );
+        }
+    }, [ selectedTaxonomy ] );
+
+    const taxonomyOptions = Object.keys( taxonomies ).map( ( key ) => {
+        return { value: key, label: taxonomies[ key ].name };
+    } );
+
+    const outputVariantOptions = [
+        { value: 'list', label: __( 'List', 'taxonomy-list-block' ) },
+        { value: 'flex', label: __( 'Flexible Blocks', 'taxonomy-list-block' ) },
+    ];
+
+    const renderTerms = () => {
+        if (outputVariant === 'list') {
+            return (
+                <ul>
+                    { terms.map( ( term ) => (
+                        <li key={ term.id }>
+                            { includeLinks ? <a href={ term.link }>{ term.name }</a> : term.name }
+                        </li>
+                    ) ) }
+                </ul>
+            );
+        } else {
+            return (
+                <div className="flex-container">
+                    { terms.map( ( term ) => (
+                        <div key={ term.id } className="flex-item">
+                            { includeLinks ? <a href={ term.link }>{ term.name }</a> : term.name }
+                        </div>
+                    ) ) }
+                </div>
+            );
+        }
+    };
+
+    return (
+        <div { ...useBlockProps() }>
+            <InspectorControls group="settings">
+                <PanelBody title={ __( 'Taxonomy Settings', 'taxonomy-list-block' ) }>
+                    <SelectControl
+                        label={ __( 'Select a taxonomy', 'taxonomy-list-block' ) }
+                        value={ selectedTaxonomy }
+                        options={ taxonomyOptions }
+                        onChange={ ( value ) => {
+                            setAttributes( { selectedTaxonomy: value } );
+                            editPost( { meta: { _selectedTaxonomy: value } } );
+                        } }
+                    />
+                    <SelectControl
+                        label={ __( 'Output Variant', 'taxonomy-list-block' ) }
+                        value={ outputVariant }
+                        options={ outputVariantOptions }
+                        onChange={ ( value ) => {
+                            setAttributes( { outputVariant: value } );
+                        } }
+                    />
+                    <ToggleControl
+                        label={ __( 'Include Links', 'taxonomy-list-block' ) }
+                        checked={ includeLinks }
+                        onChange={ ( value ) => {
+                            setAttributes( { includeLinks: value } );
+                        } }
+                    />
+                </PanelBody>
+            </InspectorControls>
+            { renderTerms() }
+        </div>
+    );
 }
